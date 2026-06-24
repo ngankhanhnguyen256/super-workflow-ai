7294469d6b0389a95afd73afcec74f54f6664a11
ngankhanhnguyen256 <ngankhanhnguyen256@gmail.com>
2026-06-24 23:26:12 +0700

    Add ReactFlow node registry (Phase 2 final step)

---
+++ lib/nodeTypes.ts
@@ -0,0 +1,8 @@
+"use client";
+
+import PromptNode from "@/components/nodes/PromptNode";
+
+export const nodeTypes = {
+  prompt: PromptNode
+  // Phase 3 will add: image, video, storyboard, stitch nodes
+};
